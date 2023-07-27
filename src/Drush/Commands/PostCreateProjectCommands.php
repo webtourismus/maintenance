ba1a86ec7f9616816a949eb9238c48916f333fd3
Webtourismus.at <entwicklung@webtourismus.at>
2023-07-27 15:27:50 +0200

    Initial commit

---
+++ src/Drush/Commands/PostCreateProjectCommands.php
@@ -0,0 +1,423 @@
+<?php
+
+namespace Drupal\maintenance\Drush\Commands;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drush\Attributes as CLI;
+use Drush\Commands\DrushCommands;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\node\Entity\Node;
+use Drupal\path_alias\Entity\PathAlias;
+use Drupal\ebr_accommodation\Entity\AccommodationBase;
+use Drupal\ebr\EntityBusinessrules;
+use Drupal\Core\Database\Connection;
+use Drupal\simple_sitemap\Manager\EntityManager as SimpleSitemapEntityManager;
+use Drupal\block_content\Entity\BlockContent;
+
+final class PostCreateProjectCommands extends DrushCommands {
+
+  /**
+   * Constructs a MaintenanceCommands object.
+   */
+  public function __construct(
+    private readonly EntityTypeManagerInterface $entityTypeManager,
+    private readonly ConfigFactoryInterface $configFactory,
+    private readonly Connection $database,
+    private readonly SimpleSitemapEntityManager $simpleSitemapEntityManager
+  ) {
+    parent::__construct();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('config.factory'),
+      $container->get('database'),
+      $container->get('simple_sitemap.entity_manager'),
+    );
+  }
+
+  /**
+   * Command description here.
+   */
+  #[CLI\Command(name: 'maintenance:create-default-content', aliases: ['def-content'])]
+  #[CLI\Usage(name: 'maintenance:create-default-content', description: 'Create default users, nodes and block_content.')]
+  public function createDefaultContent() {
+    $fileWithSensitiveDefaultContentOnLocalDevSystem = DRUPAL_ROOT . "/../../default_content.php";
+    if (file_exists($fileWithSensitiveDefaultContentOnLocalDevSystem)) {
+      include_once($fileWithSensitiveDefaultContentOnLocalDevSystem);
+    }
+
+    // Frontpage (en, de) with alias, interal_id and site setting
+    $node = Node::create([
+      'title' => 'Startseite',
+      'type' => 'page',
+      'langcode' => 'de',
+      'uid' => 1,
+      'status' => 1,
+      EntityBusinessrules::FIELD_INTERNAL_ID => 'frontpage',
+    ]);
+    // Translation
+    $node->addTranslation('en', [
+      'title' => 'Frontpage',
+      'uid' => 1,
+      'status' => 1,
+      EntityBusinessrules::FIELD_INTERNAL_ID => 'frontpage',
+    ]);
+    $node->save();
+    // Create path alias manually, pathauto disabled.
+    // To enable pathauto, use keyValue store
+    // see https://www.drupal.org/forum/support/module-development-and-code-questions/2013-04-18/how-to-programmatically-uncheck#comment-14255898
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/home',
+      'langcode' => 'de',
+    ]);
+    $nodeAlias->save();
+    // Path alias is untranslatable entity
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/home',
+      'langcode' => 'en',
+    ]);
+    $nodeAlias->save();
+    $config = $this->configFactory->getEditable('system.site');
+    $config->set('page.front', "/node/{$node->id()}")->save();
+
+    // Enquiry (en, de) with alias, interal_id
+    $node = Node::create([
+      'title' => 'Anfrage',
+      'type' => 'page',
+      'langcode' => 'de',
+      'uid' => 1,
+      'status' => 1,
+      EntityBusinessrules::FIELD_INTERNAL_ID => AccommodationBase::ACTION_ENQUIRY,
+      EntityBusinessrules::FIELD_INTERAL_NOTES => 'Öffentlich, verstecken wenn externes Anfrage-Widget. Diese Notiz löschen nach Prüfung.',
+    ]);
+    $node->addTranslation('en', [
+      'title' => 'Enquiry',
+      'uid' => 1,
+      'status' => 1,
+      EntityBusinessrules::FIELD_INTERNAL_ID => AccommodationBase::ACTION_ENQUIRY,
+      EntityBusinessrules::FIELD_INTERAL_NOTES => 'Öffentlich, verstecken wenn externes Anfrage-Widget. Diese Notiz löschen nach Prüfung.',
+    ]);
+    $node->save();
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/anfragen',
+      'langcode' => 'de',
+    ]);
+    $nodeAlias->save();
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/enquiry',
+      'langcode' => 'en',
+    ]);
+    $nodeAlias->save();
+
+    // Online booking (en, de) with alias, interal_id
+    $node = Node::create([
+      'title' => 'Online buchen',
+      'type' => 'page',
+      'langcode' => 'de',
+      'uid' => 1,
+      'status' => 1,
+      EntityBusinessrules::FIELD_INTERNAL_ID => AccommodationBase::ACTION_BOOK,
+      EntityBusinessrules::FIELD_INTERAL_NOTES => 'Muss befüllt werden. Diese Notiz löschen nach Prüfung.',
+    ]);
+    $node->addTranslation('en', [
+      'title' => 'Book online',
+      'uid' => 1,
+      'status' => 1,
+      EntityBusinessrules::FIELD_INTERNAL_ID => AccommodationBase::ACTION_BOOK,
+      EntityBusinessrules::FIELD_INTERAL_NOTES => 'Muss befüllt werden. Diese Notiz löschen nach Prüfung.',
+    ]);
+    $node->save();
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/online-buchen',
+      'langcode' => 'de',
+    ]);
+    $nodeAlias->save();
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/book-online',
+      'langcode' => 'en',
+    ]);
+    $nodeAlias->save();
+
+    // Webform Enquiry "show after submit" page
+    $node = Node::create([
+      'title' => 'Anfrage abgeschickt',
+      'type' => 'page',
+      'langcode' => 'de',
+      'uid' => 1,
+      'status' => 1,
+      EntityBusinessrules::FIELD_INTERNAL_ID => 'webform_sent_enquiry',
+      EntityBusinessrules::FIELD_INTERAL_NOTES => 'Öffentlich, verstecken wenn kein Bedarf. Diese Notiz löschen nach Prüfung.',
+      'field_seo' => '{"robots":"noindex, nofollow, noarchive, nosnippet, noimageindex"}',
+    ]);
+    $node->addTranslation('en', [
+      'title' => 'Enquiry submitted',
+      'uid' => 1,
+      'status' => 1,
+      EntityBusinessrules::FIELD_INTERNAL_ID => 'webform_sent_enquiry',
+      EntityBusinessrules::FIELD_INTERAL_NOTES => 'Öffentlich, verstecken wenn kein Bedarf. Diese Notiz löschen nach Prüfung.',
+      'field_seo' => '{"robots":"noindex, nofollow, noarchive, nosnippet, noimageindex"}',
+    ]);
+    $node->save();
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/enquiry-sent',
+      'langcode' => 'de',
+    ]);
+    $nodeAlias->save();
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/enquiry-sent',
+      'langcode' => 'en',
+    ]);
+    $nodeAlias->save();
+    $this->simpleSitemapEntityManager->setEntityInstanceSettings(
+      $node->getEntityTypeId(),
+      $node->id(),
+      ['index' => '0'],
+    );
+
+    // Webform Giftcard "show after submit" page
+    $node = Node::create([
+      'title' => 'Gutschein bestellt',
+      'type' => 'page',
+      'langcode' => 'de',
+      'uid' => 1,
+      'status' => 0,
+      EntityBusinessrules::FIELD_INTERNAL_ID => 'webform_sent_giftcard',
+      EntityBusinessrules::FIELD_INTERAL_NOTES => 'Versteckt, aktivieren bei Bedarf. Diese Notiz löschen nach Prüfung.',
+      'field_seo' => '{"robots":"noindex, nofollow, noarchive, nosnippet, noimageindex"}',
+    ]);
+    $node->addTranslation('en', [
+      'title' => 'Giftcard ordered',
+      'uid' => 1,
+      'status' => 0,
+      EntityBusinessrules::FIELD_INTERNAL_ID => 'webform_sent_giftcard',
+      EntityBusinessrules::FIELD_INTERAL_NOTES => 'Versteckt, aktivieren bei Bedarf. Diese Notiz löschen nach Prüfung.',
+      'field_seo' => '{"robots":"noindex, nofollow, noarchive, nosnippet, noimageindex"}',
+    ]);
+    $node->save();
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/giftcard-sent',
+      'langcode' => 'de',
+    ]);
+    $nodeAlias->save();
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/giftcard-sent',
+      'langcode' => 'en',
+    ]);
+    $nodeAlias->save();
+    $this->simpleSitemapEntityManager->setEntityInstanceSettings(
+      $node->getEntityTypeId(),
+      $node->id(),
+      ['index' => '0'],
+    );
+
+    // Webform Table "show after submit" page
+    $node = Node::create([
+      'title' => 'Tisch-Anfrage abgeschickt',
+      'type' => 'page',
+      'langcode' => 'de',
+      'uid' => 1,
+      'status' => 0,
+      EntityBusinessrules::FIELD_INTERNAL_ID => 'webform_sent_table',
+      EntityBusinessrules::FIELD_INTERAL_NOTES => 'Versteckt, aktivieren bei Bedarf. Diese Notiz löschen nach Prüfung.',
+      'field_seo' => '{"robots":"noindex, nofollow, noarchive, nosnippet, noimageindex"}',
+    ]);
+    $node->addTranslation('en', [
+      'title' => 'Table request submitted',
+      'uid' => 1,
+      'status' => 0,
+      EntityBusinessrules::FIELD_INTERNAL_ID => 'webform_sent_table',
+      EntityBusinessrules::FIELD_INTERAL_NOTES => 'Versteckt, aktivieren bei Bedarf. Diese Notiz löschen nach Prüfung.',
+      'field_seo' => '{"robots":"noindex, nofollow, noarchive, nosnippet, noimageindex"}',
+    ]);
+    $node->save();
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/table-sent',
+      'langcode' => 'de',
+    ]);
+    $nodeAlias->save();
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/table-sent',
+      'langcode' => 'en',
+    ]);
+    $nodeAlias->save();
+    $this->simpleSitemapEntityManager->setEntityInstanceSettings(
+      $node->getEntityTypeId(),
+      $node->id(),
+      ['index' => '0'],
+    );
+
+    // Imprint page (used by cookies module)
+    $node = Node::create([
+      'title' => 'Impressum',
+      'type' => 'page',
+      EntityBusinessrules::FIELD_INTERNAL_ID => 'imprint',
+      EntityBusinessrules::FIELD_INTERAL_NOTES => 'Muss befüllt werden. Diese Notiz löschen nach Prüfung.',
+      'langcode' => 'de',
+      'uid' => 1,
+    ]);
+    $node->save();
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/impressum',
+      'langcode' => 'de',
+    ]);
+    $nodeAlias->save();
+    $config = $this->configFactory->getEditable('cookies.texts');
+    $config->set('imprintUri', "/node/{$node->id()}")->save();
+    $menuLinkStorage = $this->entityTypeManager->getStorage('menu_link_content');
+    $menuLinkStorage->create([
+      'title' => 'Impressum',
+      'link' => ['uri' => "entity:{$node->getEntityTypeId()}/{$node->id()}"],
+      'menu_name' => 'legal',
+      'langcode' => 'de',
+      'expanded' => FALSE,
+      'weight' => 0,
+    ])->save();
+
+    // Privacy page (used by cookies module)
+    $node = Node::create([
+      'title' => 'Datenschutz',
+      'type' => 'page',
+      EntityBusinessrules::FIELD_INTERNAL_ID => 'privacy',
+      EntityBusinessrules::FIELD_INTERAL_NOTES => 'Muss befüllt werden. Diese Notiz löschen nach Prüfung.',
+      'langcode' => 'de',
+      'uid' => 1,
+    ]);
+    $node->save();
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/datenschutz',
+      'langcode' => 'de',
+    ]);
+    $nodeAlias->save();
+    $config = $this->configFactory->getEditable('cookies.texts');
+    $config->set('privacyUri', "/node/{$node->id()}")->save();
+    $menuLinkStorage = $this->entityTypeManager->getStorage('menu_link_content');
+    $menuLinkStorage->create([
+      'title' => 'Datenschutz',
+      'link' => ['uri' => "entity:{$node->getEntityTypeId()}/{$node->id()}"],
+      'menu_name' => 'legal',
+      'langcode' => 'de',
+      'expanded' => FALSE,
+      'weight' => 1,
+    ])->save();
+
+    // cookies JS ui
+    $menuLinkStorage = $this->entityTypeManager->getStorage('menu_link_content');
+    $config = $this->configFactory->getEditable('cookies.config');
+    $menuLinkStorage->create([
+      'title' => 'Cookies',
+      'link' => ['uri' => "internal:#{$config->get('open_settings_hash')}"],
+      'menu_name' => 'legal',
+      'langcode' => 'de',
+      'expanded' => FALSE,
+      'weight' => 2,
+    ])->save();
+
+    // Error 403
+    $node = Node::create([
+      'title' => 'Fehler 403 - Nicht erlaubt',
+      'type' => 'page',
+      'langcode' => 'de',
+      'uid' => 1,
+      'status' => 1,
+      EntityBusinessrules::FIELD_INTERNAL_ID => 'error_403',
+      EntityBusinessrules::FIELD_INTERAL_NOTES => 'Muss befüllt werden. Diese Notiz löschen nach Prüfung.',
+      'field_seo' => '{"robots":"noindex, nofollow, noarchive, nosnippet, noimageindex"}',
+    ]);
+    $node->save();
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/error-403',
+      'langcode' => 'de',
+    ]);
+    $nodeAlias->save();
+    $config = $this->configFactory->getEditable('system.site');
+    $config->set('page.403', "/node/{$node->id()}")->save();
+    $this->simpleSitemapEntityManager->setEntityInstanceSettings(
+      $node->getEntityTypeId(),
+      $node->id(),
+      ['index' => '0'],
+    );
+
+    // Error 404
+    $node = Node::create([
+      'title' => 'Fehler 404 - Seite nicht gefunden',
+      'type' => 'page',
+      'langcode' => 'de',
+      'uid' => 1,
+      'status' => 1,
+      EntityBusinessrules::FIELD_INTERNAL_ID => 'error_404',
+      EntityBusinessrules::FIELD_INTERAL_NOTES => 'Muss befüllt werden. Diese Notiz löschen nach Prüfung.',
+      'field_seo' => '{"robots":"noindex, nofollow, noarchive, nosnippet, noimageindex"}',
+    ]);
+    $node->save();
+    $nodeAlias = PathAlias::create([
+      'path' => "/node/{$node->id()}",
+      'alias' => '/error-404',
+      'langcode' => 'de',
+    ]);
+    $nodeAlias->save();
+    $config = $this->configFactory->getEditable('system.site');
+    $config->set('page.404', "/node/{$node->id()}")->save();
+    $this->simpleSitemapEntityManager->setEntityInstanceSettings(
+      $node->getEntityTypeId(),
+      $node->id(),
+      ['index' => '0'],
+    );
+
+    // Frontpage popup block (used by ebr_popup module)
+    $block = BlockContent::create([
+      'info' => 'Startseite Popup',
+      'type' => 'text',
+      'langcode' => 'de',
+      'reuseable' => 1,
+      'status' => 1,
+      EntityBusinessrules::FIELD_INTERNAL_ID => 'popup_frontpage',
+      'body' => [
+        'value' => '<p>Ein Popup-Fenster für die Startseite.</p>',
+        'format' => 'full_html',
+      ]
+    ]);
+    $block->addTranslation('en', [
+      'status' => 1,
+      'reuseable' => 1,
+      EntityBusinessrules::FIELD_INTERNAL_ID => 'popup_frontpage',
+      'body' => [
+        'value' => '<p>A popup window on the frontpage.</p>',
+        'format' => 'full_html',
+      ]
+    ]);
+    $block->save();
+
+    /** @var \Drupal\Core\Config\Entity\ConfigEntityInterface $blockConfig */
+    $blockConfig = $this->entityTypeManager->getStorage('block')->load('popup');
+    if ($blockConfig &&
+        $blockConfig->get('theme') == 'frontend' &&
+        $blockConfig->get('region') == 'popup' &&
+        strpos($blockConfig->get('plugin_id'), 'block_content:' === 0)
+      ) {
+      $existingUuid = str_replace('block_content:', '', $blockConfig->get('plugin_id'));
+      $block->set('uuid', $existingUuid)->save();
+    }
+
+  }
+}
---
+++ src/Robo/RoboFile.php
@@ -0,0 +1,155 @@
+<?php
+
+declare(strict_types=1);
+
+use Robo\Symfony\ConsoleIO;
+use Robo\Exception\AbortTasksException;
+
+class RoboFile extends \Robo\Tasks
+{
+
+  /**
+   * The directory pattern for all projects on the dev environment.
+   */
+  public const DEV_ENV_DIR_PATTERN = '|^/var/www/vhosts/([a-z0-9\-_]+\.webtourismus\.at)/(([a-z0-9\-_]+)\.\1)/?$|';
+
+  /**
+   * The directory pattern for all projects on the prod environment.
+   */
+  public const PROD_ENV_DIR_PATTERN = '|^/user/home/([a-z0-9\-_]+)/public_html/?$|';
+
+  public function __construct() {
+    $this->ensureProjectDir();
+  }
+
+  /**
+   * Ensures the script is run inside a project root dir on the dev server.
+   */
+  protected function ensureDevDir() {
+    if (preg_match(self::DEV_ENV_DIR_PATTERN, dirname(__FILE__)) !== 1) {
+      throw new AbortTasksException('This script must be executed in the root directory of a Drupal project on the dev server.');
+    }
+  }
+
+  /**
+   * Ensures the script is run inside a project root dir on the prod server.
+   */
+  protected function ensureProdDir() {
+    if (preg_match(self::DEV_ENV_DIR_PATTERN, dirname(__FILE__)) !== 1) {
+      throw new AbortTasksException('This script must be executed in the root directory of a Drupal project on the dev server.');
+    }
+  }
+
+  protected function ensureProjectDir() {
+    if (
+      preg_match(self::PROD_ENV_DIR_PATTERN, dirname(__FILE__)) !== 1 &&
+      preg_match(self::DEV_ENV_DIR_PATTERN, dirname(__FILE__)) !== 1
+    ) {
+      throw new AbortTasksException('This script must be executed in the root directory of a webtourismus/drupal-starterkit project.');
+    }
+  }
+
+  /**
+   * Returns the name of the dev project (without subdomain).
+   *
+   * Running this script inside "~/example.dev1.webtourismus.at/" returns "example".
+   */
+  protected function detectDevProjectName() {
+    $this->ensureDevDir();
+    preg_match(self::DEV_ENV_DIR_PATTERN, dirname(__FILE__), $results);
+    return $results[3];
+  }
+
+  /**
+   * Installs a Drupal website with default config and default data from webtourismus/drupal-starterkit.
+   */
+  public function kickoff(ConsoleIO $io) {
+    $projectName = $this->detectDevProjectName();
+    if (!file_exists('./private/scaffold/settings.default.php.append') || !file_exists('../.env')) {
+      throw new AbortTasksException('This script can only be used by webtourismus/drupal-starterkit projects.');
+    }
+    if (file_exists('./.env')) {
+      throw new AbortTasksException('A ".env" file already exists. Can\'t creata a new one.');
+    }
+    if (file_exists('./web/sites/default/settings.php')) {
+      throw new AbortTasksException('"settings.php" file found. This project already seems to be installed.');
+    }
+    $this->stopOnFail(TRUE);
+    $this->taskWriteToFile('.env')
+      ->line("PROJECT_NAME=\"{$this->detectDevProjectName()}\"")
+      ->line("DB_NAME=\"dev1_{$this->detectDevProjectName()}\"")
+      ->run();
+    $this->_exec("./vendor/bin/drush site:install --existing-config --account-name=entwicklung --account-mail=entwicklung@webtourismus.at --no-interaction");
+    $this->_exec("chmod -R u+w ./web/sites/default/");
+    $this->_exec("./vendor/bin/drush maintenance:create-default-content -y");
+    $io->say("Site {$projectName} was created.");
+    $this->_exec("git init");
+    $this->_exec("git remote add origin git@bitbucket.org:webtourismus/{$projectName}.git");
+    $this->_exec("./vendor/bin/drush config:export -y --commit --message=\"Intial commit\"");
+    $this->_exec("git push origin master");
+    $io->say("Intial commit to Bitbucket done.");
+  }
+
+  /**
+   * Like "drush config:export && git push".
+   */
+  function push(ConsoleIO $io, $message="") {
+    $this->ensureProjectDir();
+    exec("git fetch && git status", $output);
+    foreach($output as $line) {
+      if (str_contains($line, ' behind ') || str_contains($line, ' behind ')) {
+        throw new AbortTasksException('Environment is behind origin repository.');
+      }
+    }
+    if (empty($message)) {
+      $date = date('Y-m-d H:i:s');
+      $message = "Sync from {$_ENV['ENV']} on {$date}";
+    }
+    $this->stopOnFail(TRUE);
+    $this->_exec("./vendor/bin/drush config:export -y --commit --message=\"{$message}\"");
+    $this->_exec("git push origin master");
+    $io->say("Pushed to origin repository.");
+  }
+
+  /**
+   * Like "git pull && composer install && drush config:import".
+   */
+  function pull(ConsoleIO $io) {
+    $this->ensureProjectDir();
+    exec("git fetch && git status", $output);
+    foreach($output as $line) {
+      if (str_contains($line, ' ahead ') || str_contains($line, ' vor ')) {
+        throw new AbortTasksException('Environment is ahead of origin repository.');
+      }
+    }
+    exec("drush config:status", $output);
+    $dirty = TRUE;
+    foreach($output as $line) {
+      if (str_contains($line, 'No differences between DB and sync directory.')) {
+        $dirty = FALSE;
+        break;
+      }
+    }
+    if ($dirty) {
+      $answer = $io->ask('There are config changes between DB and sync directory. If you continue you\'ll loose changes in active config. Continue? (y/n) ', 'n');
+      if (strtolower($answer) !== 'y') {
+        throw new AbortTasksException('Aborted due changes in active config.');
+      }
+    }
+    $this->stopOnFail(TRUE);
+    $this->_exec("./vendor/bin/drush state:set system.maintenance_mode 1");
+    $this->_exec("git pull origin master");
+    $this->_exec("git clean -fd config/sync");
+    $this->_exec("./composer.phar install --no-dev --prefer-dist");
+    $this->_exec("./vendor/bin/drush deploy");
+    $this->_exec("./vendor/bin/drush state:set system.maintenance_mode 0");
+    $io->say("Pulled everything from origin repository.");
+  }
+
+  /**
+   * Copies a project from dev environment to prod environment.
+   */
+  public function golive(ConsoleIO $io) {
+    $io->say("Not yet implemented.");
+  }
+}
