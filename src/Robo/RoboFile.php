ccead6f2bbb956116b57f24e8fc8783771296092
Webtourismus.at <entwicklung@webtourismus.at>
2023-08-01 10:47:26 +0200

    Bug fixes

--- src/Robo/RoboFile.php
+++ src/Robo/RoboFile.php
@@ -5,15 +5,13 @@ declare(strict_types=1);
 use Robo\Symfony\ConsoleIO;
 use Robo\Exception\AbortTasksException;
 
-require_once('load.environment.php');
-
 class RoboFile extends \Robo\Tasks
 {
 
   /**
    * The directory pattern for all projects on the dev environment.
    */
-  public const DEV_ENV_DIR_PATTERN = '|^/var/www/vhosts/([a-z0-9\-_]+\.webtourismus\.at)/(([a-z0-9\-_]+)\.\1)/?$|';
+  public const DEV_ENV_DIR_PATTERN = '|^/var/www/vhosts/(([a-z0-9\-_]+)\.webtourismus\.at)/(([a-z0-9\-_]+)\.\1)/?$|';
 
   /**
    * The directory pattern for all projects on the prod environment.
@@ -54,17 +52,29 @@ class RoboFile extends \Robo\Tasks
   /**
    * Returns the name of the dev project (without subdomain).
    *
-   * Running this script inside "~/example.dev1.webtourismus.at/" returns "example".
+   * Running this script inside "~/example.dev1.webtourismus.at/" returns "example"
    */
   protected function detectDevProjectName() {
     preg_match(self::DEV_ENV_DIR_PATTERN, dirname(__FILE__), $results);
-    return $results[3];
+    return $results[4];
   }
+
   /**
-   * Creates files required for an automated Drupal installation process.
+   * Returns the name of the dev project (without subdomain).
+   *
+   * Running this script inside "~/example.dev1.webtourismus.at/" returns "dev1"
+   */
+  protected function detectDevFamilyName() {
+    preg_match(self::DEV_ENV_DIR_PATTERN, dirname(__FILE__), $results);
+    return $results[2];
+  }
+
+  /**
+   * Creates files required for an automated Drupal installation process
    */
   public function kickoffInitDev(ConsoleIO $io) {
     $this->ensureDevDir();
+    $familyName = $this->detectDevFamilyName();
     $projectName = $this->detectDevProjectName();
     if (!file_exists('./private/scaffold/default.settings.php.append') || !file_exists('../.env')) {
       throw new AbortTasksException('This script can only be used by webtourismus/drupal-starterkit projects.');
@@ -75,15 +85,17 @@ class RoboFile extends \Robo\Tasks
     if (file_exists('./web/sites/default/settings.php')) {
       throw new AbortTasksException('A "settings.php" file was found. Aborting.');
     }
+    $this->stopOnFail(TRUE);
     $this->taskWriteToFile('.env')
       ->line("PROJECT_NAME=\"{$projectName}\"")
-      ->line("DB_NAME=\"dev1_{$projectName}\"")
+      ->line("DB_NAME=\"{$familyName}_{$projectName}\"")
       ->run();
+    $this->_exec("cp ./web/sites/default/default.settings.php ./web/sites/default/settings.php");
     $io->say("Created minimal env and settings file for dev system.");
   }
 
   /**
-   * Installs a Drupal website with default config and default data from webtourismus/drupal-starterkit.
+   * Installs a Drupal website with default config and default data from webtourismus/drupal-starterkit
    */
   public function kickoffInstallDrupal(ConsoleIO $io) {
     $this->ensureDevDir();
@@ -96,40 +108,40 @@ class RoboFile extends \Robo\Tasks
     }
     $this->stopOnFail(TRUE);
     $this->_exec("chmod -R u+w ./web/sites/default");
-    $this->_exec("cp ./web/sites/default/default.settings.php ./web/sites/default/settings.php");
-    // Drush's install routine can't access $_ENV from composer autoloader (unknown bug?).
-    // So robo (which does know $_ENV) passes them as hardcoded values to the CLI.
+    // .env variable autoloading does not work when Drush is called through Robo
+    // @see https://github.com/consolidation/robo/issues/1148
     $this->_exec("./vendor/bin/drush site:install --existing-config --site-name=\"{$projectName}\" --account-name=entwicklung --account-mail=entwicklung@webtourismus.at --db-url=\"mysql://{$_ENV['DB_USER']}:{$_ENV['DB_PASS']}@{$_ENV['DB_HOST']}:{$_ENV['DB_PORT']}/{$_ENV['DB_NAME']}\" --no-interaction");
     $this->_exec("chmod -R u+w ./web/sites/default");
     // Convert hardcoded credentials from CLI back into $_ENV settings.
     $this->taskReplaceInFile('./web/sites/default/settings.php')
       ->from([
-        " 'database' => '{$_ENV['DB_NAME']}',",
-        " 'username' => '{$_ENV['DB_USER']}',",
-        " 'password' => '{$_ENV['DB_PASS']}',",
-        " 'host' => '{$_ENV['DB_HOST']}',",
-        " 'port' => '{$_ENV['DB_PORT']}',",
+        "'database' => '{$_ENV['DB_NAME']}',",
+        "'username' => '{$_ENV['DB_USER']}',",
+        "'password' => '{$_ENV['DB_PASS']}',",
+        "'host' => '{$_ENV['DB_HOST']}',",
+        "'port' => '{$_ENV['DB_PORT']}',",
       ])
       ->to([
-        " 'database' => \$_ENV['DB_NAME'],",
-        " 'username' => \$_ENV['DB_USER'],",
-        " 'password' => \$_ENV['DB_PASS'],",
-        " 'host' => \$_ENV['DB_HOST'] ?? 'localhost',",
-        " 'port' => \$_ENV['DB_PORT'] ?? 3306,",
+        "'database' => \$_ENV['DB_NAME'],",
+        "'username' => \$_ENV['DB_USER'],",
+        "'password' => \$_ENV['DB_PASS'],",
+        "'host' => \$_ENV['DB_HOST'] ?? 'localhost',",
+        "'port' => \$_ENV['DB_PORT'] ?? 3306,",
       ])
       ->run();
-    $this->_exec("./vendor/bin/drush maintenance:create-default-content -y");
+    // @TODO script currently breaks here because due .env bug from above
     $this->_exec("./vendor/bin/drush cache:rebuild");
+    $this->_exec("./vendor/bin/drush maintenance:create-default-content -y");
     $io->say("Site {$projectName} was created.");
     $this->_exec("git init");
     $this->_exec("git remote add origin git@bitbucket.org:webtourismus/{$projectName}.git");
     $this->_exec("./vendor/bin/drush config:export -y --commit --message=\"Initial commit\"");
     $this->_exec("git push origin master");
-    $io->say("Intial commit to Bitbucket done.");
+    $io->say("Initial commit to Bitbucket done.");
   }
 
   /**
-   * Like "drush config:export && git push".
+   * Like "drush config:export && git push"
    */
   function push(ConsoleIO $io, $message="") {
     $this->ensureProjectDir();
@@ -153,7 +165,7 @@ class RoboFile extends \Robo\Tasks
   }
 
   /**
-   * Like "git pull && composer install && drush config:import".
+   * Like "git pull && composer install && drush config:import"
    */
   function pull(ConsoleIO $io) {
     $this->ensureProjectDir();
@@ -188,7 +200,7 @@ class RoboFile extends \Robo\Tasks
   }
 
   /**
-   * Copies a project from dev environment to prod environment.
+   * Copies a project from dev environment to prod environment (not yet implemented)
    */
   public function golive(ConsoleIO $io) {
     $io->say("Not yet implemented.");
